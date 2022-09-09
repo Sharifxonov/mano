024e4e4bdb71c41059ea39d184b3b2e5e060d792
temurxon <113027702+Sharifxonov@users.noreply.github.com>
2022-09-09 14:21:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,109 @@
+// 1
+
+// let a = 10;
+// for(i = 0; i <= a; i++){
+//  console.log(a)
+// }
+
+// 2
+
+// let a = 5, b = 10;
+// for(j = a; j <= b; j++){
+//     console.log(j);
+// }
+
+// 3
+
+// let a = -1;
+// for(i = 10; a < i; i--){
+//     console.log(`${i}`);
+// }
+
+// 4
+
+// for(j = 0; j <= 10; j++){
+//     console.log(`${j}kg + olma = ${j*2000}so'm`);
+// }
+
+// 5
+
+// let a = 50000;
+// for(i = 1; i <= 10; i++){
+//     console.log((`${i/10}kanfet = ${i/10*a}so'm`));
+// }
+
+// 6
+
+// let a = 50000;
+// for(i = 12; i <= 20; i++){
+//     console.log((`${i/10}kanfet = ${i/10*a}so'm`));
+// }
+
+// 7
+
+// let a = 20;
+// for(i = 2; i<20; i++){
+//     a +=i
+//     console.log(a);
+// }
+
+// 8
+
+// let a = 10;
+// for(i = 2; i<20; i++){
+//     a *=i
+//     console.log(a);
+// }
+
+// 9
+
+//  let a = 0;
+//  for(i = 2; i<=3; i++){
+//      a +=i**2
+    
+//  }console.log(a);
+
+// 10
+
+// let a = 0;
+// for(let n = 1; n <=10; n++){
+//     a +=1/n 
+// }console.log(a);
+
+// 11
+
+// let s=0,n=4;
+// for (let a = 0; a < n; a++) {
+//     s+=(n+a)**2    
+// }    console.log(s);
+
+// 12
+
+// let s=1;
+// for (let n = 11; n < 20; n++) {
+//     s*=n/10
+// }    console.log(s);
+
+// 13
+
+// let s=0
+// for (let n = 6; n < 8; n++) {
+//     s+= (n*2-1)/10-(n*2)/10    
+// }console.log(s);
+
+// 14
+
+// let s=0
+// for (let n = 1; n <= 3; n++) {
+//     s+= ((n*2-1)) **2 
+// }console.log(s);
+
+// 15
+
+// let s=1;let n=4;
+// for (let a = 2; a < 3; a++) {
+//     s*=a**n
+// }   console.log(s);
+
+let a=5,b=8,c=1;
+console.log(Math.max);
